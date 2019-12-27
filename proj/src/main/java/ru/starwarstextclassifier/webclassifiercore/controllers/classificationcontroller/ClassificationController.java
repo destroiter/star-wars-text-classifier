74df0e9242d7f8505283295c4fd3cfeb68a915de
Fadeev Mikhail <Fadeev.mix1997@gmail.com>
2019-12-27 19:26:42 +0300

    Обработка обращения к классификатору с пустым запросом

--- proj/src/main/java/ru/starwarstextclassifier/webclassifiercore/controllers/classificationcontroller/ClassificationController.java
+++ proj/src/main/java/ru/starwarstextclassifier/webclassifiercore/controllers/classificationcontroller/ClassificationController.java
@@ -4,10 +4,12 @@ import ru.starwarstextclassifier.webclassifiercore.controllers.classificationcon
 import ru.starwarstextclassifier.webclassifiercore.controllers.classificationcontroller.requests.ClassifyRequest;
 import ru.starwarstextclassifier.webclassifiercore.services.ClassificationService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/api/classification")
@@ -20,6 +22,9 @@ public class ClassificationController {
     
     @PostMapping(value = "/classify")
     public ClassifyResponse classify(@RequestBody ClassifyRequest request) throws Exception {
+        if (request.getText().trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Empty string cannot be classified");
+        }
         return new ClassifyResponse(classificationService.classify(request.getText()));
     }
 }
